299f83279ac2ffada896faa7311583943a8bcaab
Evgeny <chiburashka47@gmail.com>
2020-06-11 21:04:29 +0300

    mainPage  fix

--- src/Components/Body/Components/MainPage/mainPage.js
+++ src/Components/Body/Components/MainPage/mainPage.js
@@ -3,6 +3,15 @@ import "./style/mainPage.scss";
 
 export default class MainPage extends Component {
   render() {
-    return <div className="mainPageContainer">123</div>;
+    return (
+      <div className="mainPageContainer">
+        <div className="mainPageTitile">
+          Главная / LIVE / Counter-StrikeLOOTBET / Season 6.
+        </div>
+        <div className="mainPageEvent"></div>
+        <div className="mainPageNews"></div>
+        <div className="mainPageMeny"></div>
+      </div>
+    );
   }
 }
